628c7e515b19285b3cf16286ff0a6cd3ad018b13
Mohit Paudel <39890575+MohitP197@users.noreply.github.com>
2020-04-05 22:50:51 +0545

    Added QuoteCard to a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './model/quote.dart';
+import 'class/quote_cart.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -40,48 +41,9 @@ class _QuoteListsState extends State<QuoteLists> {
       ),
       body: Column(
         // Cycle through the quotes list and fire a function for each quote and for each quote
-        // get a new instance of the quote card which is returning Widget and outputing quote data 
+        // get a new instance of the quote card which is returning Widget and outputing quote data
         // Curly braces are used to wrap the value when the items are in list
         children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
-      ),
-    );
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-  final Quote quote;
-
-  // Named parameter is received and assigning to above local variable
-  QuoteCard({this.quote});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(
-              height: 6.0,
-            ),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            )
-          ],
-        ),
       ),
     );
   }
